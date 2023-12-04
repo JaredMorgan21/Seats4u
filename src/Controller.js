4a5b7412425967f3d5c1ce54f06163f395c50592
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2023-12-04 14:46:31 -0500

    Update Controller.js

--- src/Controller.js
+++ src/Controller.js
@@ -47,7 +47,7 @@ export function createShow(venueName, password, title, startTime, endTime, usesB
 
 //TODO something is wrong with this function (doesn't work locally)
 export function createBlock(name, password, startTime, section, startRow, endRow, price) {
-    let data = {  "name": name,
+    let data = {  "venueName": name,
                   "password" : password,
                   "startTime" : startTime,
                   "section" : section,
@@ -58,7 +58,7 @@ export function createBlock(name, password, startTime, section, startRow, endRow
     const handler = (json) => {
         console.log(json)
         if(json.statusCode == 200){
-            document.getElementById("createBlockResult").innerHTML = "The following blocks have been created: [insert blocks created here]" //TODO 
+            document.getElementById("createBlockResult").innerHTML = "The following blocks have been created: \'" + json.success + "\'" //TODO 
         }
         else{
             document.getElementById("createBlockResult").innerHTML = "Error: " + json.error
@@ -68,8 +68,25 @@ export function createBlock(name, password, startTime, section, startRow, endRow
     post('/show/createBlock', data, handler)
 }
 
-export function deleteBlock() {
 
+export function deleteBlock(name, password, startTime, section, startRow, endRow) {
+    let data = {  "venueName": name,
+                  "password" : password,
+                  "startTime" : startTime,
+                  "section" : section,
+                  "startRow" : startRow,
+                  "endRow" : endRow}
+                  
+    const handler = (json) => {
+        console.log(json)
+        if(json.statusCode == 200){
+            document.getElementById("deleteBlockResult").innerHTML = "The following blocks have been deleted: \'" + json.success + "\'" //TODO 
+        }
+        else{
+            document.getElementById("deleteBlockResult").innerHTML = "Error: " + json.error
+        }
+    }
+    post('/show/deleteBlock', data, handler)
 }
 
 export function activateShow(name, password, title, startTime) {
